7428429287a2256768a0baa95e022db32e1bd7d9
meysamnorouzi <work.meysamnorouzi@gmail.com>
2022-08-08 19:18:20 +0430

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
+import Img from "./breeze.jpg";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Hello World</h1>
+      <img src={Img} alt="Breeze" />
     </div>
   );
 }
