c14cf0a0605f2fddd958f34856ee85275fdc7362
meysamnorouzi <work.meysamnorouzi@gmail.com>
2022-08-08 18:38:44 +0430

    Update webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -19,6 +19,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
